64ff9e879525353c341a333f65e0e8c27cea26ca
Abraham Saldana <abraham105@hotmail.com>
2021-05-09 20:27:09 -0500

    vue form generator

---
+++ resources/js/components/resume/ResumeForm.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <ResumeForm />
+  </div>
+</template>
+
+<script>
+import VueFormGenerator from 'vue-form-generator';
+import 'vue-form-generator/dist/vfg.css';
+
+export default {
+  name: 'ResumeForm',
+
+};
+</script>

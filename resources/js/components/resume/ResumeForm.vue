c6ff9890f79b367344af5243c1f4e96b8b54f0b5
Abraham Saldana <abraham105@hotmail.com>
2021-05-10 00:28:26 -0500

    Librerias bootstrap y tabs

--- resources/js/components/resume/ResumeForm.vue
+++ resources/js/components/resume/ResumeForm.vue
@@ -1,15 +1,34 @@
 <template>
   <div>
-    <ResumeForm />
+      <Tabs>
+          <Tab title="Test" icon="fas fa-user">
+              <h1>Test Primero</h1>
+          </Tab>
+
+          <Tab title="otro">
+              <h1>Test Dos</h1>
+          </Tab>
+          <Tab title="otro">
+              <h1>Test Dos</h1>
+          </Tab>
+          <Tab title="otro">
+              <h1>Test Dos</h1>
+          </Tab>
+          <Tab title="otro">
+              <h1>Test Dos</h1>
+          </Tab>
+      </Tabs>
   </div>
 </template>
 
 <script>
-import VueFormGenerator from 'vue-form-generator';
-import 'vue-form-generator/dist/vfg.css';
-
+import Tabs from './tabs/Tabs';
+import Tab from './tabs/Tab';
 export default {
   name: 'ResumeForm',
-
+  components:{
+      Tabs,
+      Tab,
+  },
 };
 </script>
